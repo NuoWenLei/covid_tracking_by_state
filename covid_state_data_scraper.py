924803eff911b2893c1fc83d2c4c739f81f2dec4
NuoWenLei <nuowen0612@gmail.com>
2021-03-05 20:58:44 +0800

    re-added correct state data as well as updated webscraper script

--- covid_state_data_scraper.py
+++ covid_state_data_scraper.py
@@ -5,10 +5,11 @@ r = requests.get('https://covidtracking.com/data')
 soup = BeautifulSoup(r.text, 'html.parser')
 state_links = [[i['href'].split('/')[-1], f"https://covidtracking.com{i['href']}"] for i in soup.find_all('a', string='Full state data including data sources and notes')]
 for state, link in state_links:
-	r = requests.get(link)
+	r = requests.get(f'https://covidtracking.com/data/download/{state}-history.csv')
 	soup = BeautifulSoup(r.text, 'html.parser')
-	with open('{}.csv'.format(state), 'w') as state_writer:
-		state_writer.write(soup.find_all('p')[0].text)
+	with open('state_data/{}.csv'.format(state), 'w') as state_writer:
+		print(soup.text)
+		state_writer.write(soup.text)
 		state_writer.close()
 	print(state)
 
